eb3ccab3538a2cfff76034dc44db0e0f912b19e5
Yordan <pm.yordan@gmail.com>
2019-01-10 15:00:18 +0200

    Added validation attribute on comment and commentinputModel.

--- src/WinnersLeague.Models/Comments.cs
+++ src/WinnersLeague.Models/Comments.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@
 
         virtual public WinnersLeagueUser Author { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Content { get; set; }
 
         public string Title { get; set; }
--- src/WinnersLeague.Web/Models/CommentInputModel.cs
+++ src/WinnersLeague.Web/Models/CommentInputModel.cs
@@ -1,9 +1,13 @@
-﻿namespace WinnersLeague.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WinnersLeague.Web.Models
 {
     public class CommentInputModel
     {
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Content { get; set; }
 
         public string MatchId { get; set; }
